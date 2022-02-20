0c70f1a19e4d75a01d2b04e929ea6c3d06167673
nadiaH <nadiahrb79@gmailcom>
2022-02-20 11:53:47 +0200

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/table">Add</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="table" element={<h1>Table</h1>} />
+
+        <Route
+          path="*"
+          element={(
+            <p>
+              Page not found
+            </p>
+          )}
+        />
+      </Routes>
     </div>
   );
 };
